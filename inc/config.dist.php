8521729a0f17505b323efe532cc20df493756c6c
Sam <davilex_18@hotmail.com>
2017-11-13 15:56:25 +0100

    added config.dist.php

---
+++ inc/config.dist.php
@@ -0,0 +1,12 @@
+<?php
+// Config data
+$config = [
+    'DB_HOST' => '',
+    'DB_NAME' => '',
+    'DB_USER' => '',
+    'DB_PASS' => ''
+];
+
+// Include files
+require_once __DIR__ . '/db.php';
+require_once __DIR__ . '/functions.php';
